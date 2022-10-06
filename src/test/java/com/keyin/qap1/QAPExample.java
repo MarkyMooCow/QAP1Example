052a301a07d6d94ac3fb0c9be6308c967debbc4a
Mark Hunter <95723859+MarkyMooCow@users.noreply.github.com>
2022-10-05 23:45:29 -0230

    Delete QAPExample.java

--- src/test/java/com/keyin/qap1/QAPExample.java
+++
@@ -1,7 +0,0 @@
-package com.keyin.qap1;
-
-public class QAPExample {
-    public boolean findSequenceInWord(String wordToFind, String charSequence) {
-        return charSequence.contains(wordToFind);
-    }
-}
